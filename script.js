f411fd28044e4f192680feb11c4582442eb0cc55
SaziaMakkar19 <78702980+SaziaMakkar19@users.noreply.github.com>
2023-01-13 14:45:44 -0800

    Script.js Modified

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'http://localhost:3000/chatbot',
+			url: 'https://gpt-4-chatbot-itr5.onrender.com',
 			data: {
 				message: message
 			},
